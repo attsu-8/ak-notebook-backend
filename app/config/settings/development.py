550a3accaa7301201095028a138202174d9b93e0
atsuzawa <atsuzawa.k8@gmail.com>
2022-04-13 01:59:06 +0900

    add: プロフィール画像をS3へ格納する機能を追加

--- app/config/settings/development.py
+++ app/config/settings/development.py
@@ -13,3 +13,6 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:13000',
     'http://localhost:13000',
 ]
+
+MEDIA_URL = '/media/'
+
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,4 +1,4 @@
-from config.settings.os_env import SECRET_KEY
+from config.settings.os_env import SECRET_KEY, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_LOCATION
 from .base import *
 
 SECRET_KEY = SECRET_KEY
@@ -13,3 +13,15 @@ CORS_ORIGIN_WHITELIST = [
     'https://ak-notebook.com',
     'https://ak-notebook.com:10443',
 ]
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME
+AWS_LOCATION = AWS_LOCATION # s3バケット上のベースとなるファイルパス
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
